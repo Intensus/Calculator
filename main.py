c7d8109daef9d64865bb7d9e90c2ec5ef206d8d0
Andreas Panagiotidis <annpanagiotidis@gmail.com>
2023-09-05 20:07:02 +0300

    Use operation function from dictionary. Added comments.

--- main.py
+++ main.py
@@ -17,31 +17,39 @@ def divide(x, y):
     return x/y
 
 
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+    }
 rerun = True
 use_result = "n"
 
 print(logo)
 while rerun:
+
+    # Get the first number.
+    # In case of a rerun, do we use the result from last time?
     if use_result == "n":
         n1 = float(input("What is the first number? "))
     else:
         print(f"First number: {n1}")
+
+    # Choose the kind of operation
     action = ""
-    while not action == "+" and not action == "-" and not action == "*" and not action == "/":
+    while action not in operations:
         action=input("Pick an operation (+ - * /) ")
+    operation = operations[action] # operation points to relevant function
+
+    # Get the second number.
     n2=float(input("What is the next number? "))
-    match action:
-        case "+":
-            result=add(n1, n2)
-        case "-":
-            result = subtract(n1, n2)
-        case "*":
-            result = multiply(n1, n2)
-        case "/":
-            result = divide(n1, n2)
-        case _:
-            result = "Undefined result"
+
+    # Print result.
+    result = operation(n1, n2)
     print(f"{n1} {action} {n2} = {result}")
+
+    # Use result for next calculation "y/n" or quit "q"?
     use_result=""
     while not use_result == "y" and not use_result == "n" and not use_result == "q":
         use_result=input(f"Type Y to continue working with {result}, type N to start a new calculation or Q to quit. ").lower()
